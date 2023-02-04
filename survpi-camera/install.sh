c8b742e1913772e990b6dc2f6d0b1eb7103f5e8d
BillPlayzToday <tonileoels@gmail.com>
2023-02-04 21:07:36 +0100

    Update install.sh

--- survpi-camera/install.sh
+++ survpi-camera/install.sh
@@ -1,1 +1,16 @@
-mv -f /home/survpi-camera/survpi.service /etc/systemd/system/
+if [ "$EUID" != "0" ]
+    then echo "Please run the install script as root or sudo."
+    exit
+fi
+
+echo "Updating packages..."
+apt update
+apt upgrade -y
+
+echo "Installing git, if not installed."
+apt install git -y
+
+echo "Cloning camera directory."
+git clone --depth 1 --filter=blob:none --sparse https://github.com/BillPlayzToday/SurveillancePi
+cd SurveillancePi
+git sparse-checkout set survpi-camera
