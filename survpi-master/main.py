04b819bc630d14970d81c381a340280d1595e50a
BillPlayzToday <tonileoels@gmail.com>
2023-02-26 11:37:54 +0100

    Fix the stupidness

--- survpi-master/main.py
+++ survpi-master/main.py
@@ -14,12 +14,11 @@ def broadcastThread():
     udpSocket.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST,1)
     try:
         while True:
-            udpSocket.sendto(b"survpi-master!ready-recv",("",8887))
+            udpSocket.sendto(b"survpi-master!ready-recv",("192.168.178.255",8887))
             time.sleep(3)
             print("sent ping")
     except KeyboardInterrupt:
         udpSocket.close()
-        print("Closed UDP socket")
 
 if (__name__ == "__main__"):
     udpBroadcaster = multiprocessing.Process(
@@ -27,5 +26,5 @@ if (__name__ == "__main__"):
     )
     udpBroadcaster.start()
 
-    tcpServer = socketserver.TCPServer(("192.168.178.255",8888),SocketHandler)
+    tcpServer = socketserver.TCPServer(("0.0.0.0",8888),SocketHandler)
     tcpServer.serve_forever()
