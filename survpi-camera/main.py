dc3083a552e3024edc07681d5a617f752d9ee646
BillPlayzToday <tonileoels@gmail.com>
2023-02-27 17:58:48 +0100

    Add (some) Socket Functionality + Add reinstall to camera

--- survpi-camera/main.py
+++ survpi-camera/main.py
@@ -53,7 +53,7 @@ while True:
 
     lastFileLength = 0
     os.remove("/home/pi/survpi-output/current.h264")
-    masterSocket.send(b"")
+    masterSocket.sendall(b"survpi-camera!ready-send")
     while True:
         currentTime = time.time()
         if (currentTime - streamStart >= 1200):
--- survpi-master/main.py
+++ survpi-master/main.py
@@ -5,11 +5,28 @@ import multiprocessing
 import time
 
 configFile = "/home/pi/SurveillancePi/survpi-master/config.json"
+pendingData = {}
 
 class SocketHandler(socketserver.StreamRequestHandler):
     def handle(self):
-        self.data = self.request.recv(1024)
-        self.client_address
+        print(f"[{self.client_address}] Blocking...")
+        self.data = self.request.recv()
+        if (self.data == b"survpi-camera!ready-send"):
+            print(f"[{self.client_address}] Ready.")
+            pendingData[self.client_address] = b""
+        else:
+            if (not pendingData.get(self.client_address)):
+                print(f"[{self.client_address}] Closing, no entry.")
+                self.connection.close()
+                return
+            print(f"[{self.client_address}] Appending {str(len(self.data))} bytes.")
+            pendingData[self.client_address] = pendingData[self.client_address] + self.data
+    
+    def finish(self):
+        if (not pendingData.get(self.client_address)):
+            print(f"[{self.client_address}] Ignoring disconnect, no entry.")
+            return
+        print(f"[{self.client_address}] Disconnected with {str(len(pendingData[self.client_address]))} bytes.")
 
 
 def getConfigData():
@@ -28,7 +45,7 @@ def broadcastThread():
                 try:
                     udpSocket.sendto(b"survpi-master!ready-recv",("192.168.178.255",8887))
                 except OSError as exception:
-                    if (exception.errno != 101): # not Network unreachable
+                    if (exception.errno != 101):
                         raise
             time.sleep(configData.get("broadcastInterval"))
     except KeyboardInterrupt:
