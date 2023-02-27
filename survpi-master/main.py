35551079bea7c14a79a95a631426fa6c7af4b65b
BillPlayzToday <tonileoels@gmail.com>
2023-02-27 16:13:57 +0100

    Add config.json + Server startup fix

--- survpi-master/main.py
+++ survpi-master/main.py
@@ -1,22 +1,36 @@
 import socketserver
 import socket
-import signal
+import json
 import multiprocessing
 import time
 
+configFile = "/home/SurveillancePi/survpi-master/config.json"
+
 class SocketHandler(socketserver.StreamRequestHandler):
     def handle(self):
-        pass
+        self.data = self.request.recv(1024)
+        self.client_address
 
+
+def getConfigData():
+    openFile = open(configFile)
+    configData = json.loads(openFile.read())
+    openFile.close()
+    return configData
 
 def broadcastThread():
     udpSocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM,socket.IPPROTO_UDP)
     udpSocket.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST,1)
     try:
         while True:
-            udpSocket.sendto(b"survpi-master!ready-recv",("192.168.178.255",8887))
-            time.sleep(3)
-            print("sent ping")
+            configData = getConfigData()
+            if (configData.get("doBroadcast")):
+                try:
+                    udpSocket.sendto(b"survpi-master!ready-recv",("192.168.178.255",8887))
+                except OSError as exception:
+                    if (exception.errno != 101): # not Network unreachable
+                        raise
+            time.sleep(configData.get("broadcastInterval"))
     except KeyboardInterrupt:
         udpSocket.close()
 
